0d1d195c1dac2b8530285f9f77253b6be75429ef
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-07-02 19:52:22 -0400

    pages structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./Pages/App/index.jsx";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
