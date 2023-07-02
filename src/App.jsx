41fd332438f6b83e0ce2a3eebd05e90fe4dd7814
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-07-02 19:28:03 -0400

    configuring the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full bg-orange-500 h-8">klk entonces</div>
+      <h1 className="text-blue-800">Vite + React + Tailwind</h1>
+    </>
+  );
+}
+
+export default App;
