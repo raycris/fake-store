fb6dba8c4aef9460e6448c492a58c7d8a8bff570
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-07-09 15:39:05 -0400

    adding icons around the project

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 
-import {ShoppingCartContext} from "../../context";
+import { ShoppingCartContext } from "../../context";
 
 const Navbar = () => {
-  const context = useContext(ShoppingCartContext)
+  const context = useContext(ShoppingCartContext);
   const activeStyle = "underline underline-offset-4";
   return (
     <nav className="flex w-full min-w-[280px] justify-between items-center fixed z-10 top-0 text-sm font-light p-3">
@@ -95,7 +96,10 @@ const Navbar = () => {
             Sign In
           </NavLink>
         </li>
-        <li>🛒 {context.count}</li>
+        <li className="flex items-center">
+          <ShoppingBagIcon className="h-6 w-6 text-black" />
+          <div>{context.count}</div>
+        </li>
       </ul>
     </nav>
   );
